3b6a19682b756bae7843becec3e90aaca3aa0fee
annecie <ajbexcelsior@gmail.com>
2022-05-11 18:42:32 -0400

    homepage updated

--- fare-dash-nyc/src/App.js
+++ fare-dash-nyc/src/App.js
@@ -1,13 +1,24 @@
-// import React from "react"
-// import './App.css';
-// import navbar from "./Components/navBar";
+import React from "react"
+import './App.css';
 
 function App() {
-  return (
-  <div>
-    <h1> Fare Dash NYC</h1>
-  </div>
-);
+	return (
+		<div>
+      <h1> FARE DASH </h1>
+			<nav class="navbar background">
+				<ul class="nav-list">
+					<li><a href="#explore">Explore</a></li>
+					<li><a href="#login">Login</a></li>
+					<li><a href="#signup">Sign Up</a></li>
+				</ul>
+
+				<div class="rightNav">
+					<input type="text" name="search" id="search" />
+					<button class="btn btn-sm">Search</button>
+				</div>
+			</nav>
+		</div>
+	)
 }
 
-export default App;
+export default App
