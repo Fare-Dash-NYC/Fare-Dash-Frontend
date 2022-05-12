c0023ae1f03fdc33c9eb39b6975b970967444e3c
Khalia Parris <khaliaparris01@gmail.com>
2022-05-11 23:01:01 -0400

    added main routes and started sign up/ login page

--- fare-dash-nyc/src/Components/login.js
+++ fare-dash-nyc/src/Components/login.js
@@ -1,9 +1,48 @@
+
+import {Button,Label,Input,FormGroup,Form} from 'reactstrap'
+import {Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Login(){
 
 
     return(
          <div>
-             LOG IN
+           <Form inline>
+               <h1>LOG IN</h1>
+  <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+    <Label
+      className="me-sm-2"
+      for="exampleEmail"
+    >
+      Email
+    </Label>
+    <Input
+      id="exampleEmail"
+      name="email"
+      placeholder="something@idk.cool"
+      type="email"
+    />
+  </FormGroup>
+  <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+    <Label
+      className="me-sm-2"
+      for="examplePassword"
+    >
+      Password
+    </Label>
+    <Input
+      id="examplePassword"
+      name="password"
+      placeholder="don't tell!"
+      type="password"
+    />
+  </FormGroup>
+    <Link to={'/explore'}>
+  <Button>
+    Log in 
+  </Button>
+  </Link>
+</Form>
          </div>
     )
 }
