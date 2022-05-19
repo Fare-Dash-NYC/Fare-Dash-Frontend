8cfb9ff2fdf5dba438107a773bfa54942a124108
annecie <ajbexcelsior@gmail.com>
2022-05-19 16:05:05 -0400

    layout forum

--- fare-dash-nyc/src/Components/PostBoard.js
+++ fare-dash-nyc/src/Components/PostBoard.js
@@ -15,8 +15,8 @@ const postBoard = () => {
           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         name: 'John Doe',
         content:
-          '<p>Raising say express had chiefly detract demands she. Quiet led own cause three him. Front no party young abode state up. Saved he do fruit woody of to. Met defective are allowance two perceived listening consulted contained. It chicken oh colonel pressed excited suppose to shortly. He improve started no we manners however effects. Prospect humoured mistress to by proposal marianne attended. Simplicity the far admiration preference everything. Up help home head spot an he room in Barton waited twenty always repair in within we do. An delighted offending curiosity my is dashwoods at. Boy prosperous increasing surrounded companions her nor advantages sufficient put. John on time down give meet help as of. Him waiting and correct believe now cottage she another. Vexed six shy yet along learn maids her tiled. Through studied shyness evening bed him winding present. Become excuse hardly on my thirty it wanted. </p>',
-        date: new Date('01 Jan 2020 09:12:00 GMT')
+          '<p> Im hungry </p>',
+        date: new Date('19 May 2022 ')
       }
     ]
   
