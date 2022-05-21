2a5d6da800130ff62818223e17422143ab87486c
Jan <moralesjan40@gmail.com>
2022-05-21 17:59:18 -0400

    login page talks to server and gets token

--- fare-dash-nyc/src/Components/login.js
+++ fare-dash-nyc/src/Components/login.js
@@ -1,64 +1,71 @@
+import { Button, Label, Input, FormGroup, Form } from "reactstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import {Button,Label,Input,FormGroup,Form} from 'reactstrap'
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+const url = "http://localhost:8081";
 
+function Login() {
+  const navigate = useNavigate();
+  const [LogInForm, setLogInForm] = useState({
+    email: "",
+    password: "",
+  });
+  const handleLogInFormChange = (event) => {
+    event.preventDefault();
+    setLogInForm({
+      ...LogInForm,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-function Login(){
- const [email, setEmail] = useState('')
- const[password, setPassword] = useState('')
+  const handleLogIn = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`${url}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(LogInForm),
+    });
+    const data = await response.json();
+    console.log(data.token);
+    navigate("/explore", { replace: true });
+  };
 
-
-
-
-
-
-    return(
-         <div>
-           <Form inline>
-               <h1>LOG IN</h1>
-  <FormGroup className="mb-2 me-sm-2 mb-sm-0">
-    <Label
-      className="me-sm-2"
-      for="exampleEmail"
-    >
-      Email
-    </Label>
-    <Input
-     onChange={((event )=>{
-      setEmail(event.target.value)
-    })}
-      id="exampleEmail"
-      name="email"
-      placeholder="something@idk.cool"
-      type="email"
-    />
-  </FormGroup>
-  <FormGroup className="mb-2 me-sm-2 mb-sm-0">
-    <Label
-      className="me-sm-2"
-      for="examplePassword"
-    >
-      Password
-    </Label>
-    <Input
-     onChange={((event )=>{
-      setPassword(event.target.value)
-    })}
-      id="examplePassword"
-      name="password"
-      placeholder="don't tell!"
-      type="password"
-    />
-  </FormGroup>
-    <Link to={'/explore'}>
-  <Button>
-    Log in 
-  </Button>
-  </Link>
-</Form>
-         </div>
-    )
+  return (
+    <div>
+      <Form inline onSubmit={handleLogIn}>
+        <h1>LOG IN</h1>
+        <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+          <Label className="me-sm-2" for="exampleEmail">
+            Email
+          </Label>
+          <Input
+            onChange={handleLogInFormChange}
+            id="exampleEmail"
+            name="email"
+            placeholder="something@idk.cool"
+            type="email"
+          />
+        </FormGroup>
+        <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+          <Label className="me-sm-2" for="examplePassword">
+            Password
+          </Label>
+          <Input
+            onChange={handleLogInFormChange}
+            id="examplePassword"
+            name="password"
+            placeholder="don't tell!"
+            type="password"
+          />
+        </FormGroup>
+        <Button type="submit">Log in</Button>
+      </Form>
+    </div>
+  );
 }
-export default Login
+export default Login;
